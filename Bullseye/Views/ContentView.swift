528d66f9c2668bc06e278b3dd05035401852145b
a1mond <kotik.slava2012@gmail.com>
2021-05-30 01:45:38 +0200

    refactoring

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -16,52 +16,65 @@ struct ContentView: View {
             Color("BackgroundColor")
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack {
-                Text("Put the bullseye as close as you can to".uppercased())
-                    .bold()
-                    .kerning(2.0)
-                    .multilineTextAlignment(.center)
-                    .lineSpacing(4.0)
-                    .font(.footnote)
-                    .padding()
-                
-                Text(String(game.target))
-                    .kerning(-1.0)
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                
-                HStack {
-                    Text("1")
-                        .bold()
-                    Slider(value: $sliderValue, in: 1.0...100.0)
-                    Text("100")
-                        .bold()
-                }
-                .padding()
-                
-                Button(action: {
-                    alertIsVisible = true
-                    print("Button is pressed.")
-                }) {
-                    Text("submit".uppercased()).bold().font(.title3)
-                }
-                .padding(20.0)
-                .background(
-                    ZStack {
-                        Color("ButtonColor")
-                        LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
-                    }
-                )
-                .foregroundColor(Color.white)
-                .cornerRadius(21.0)
-                .alert(isPresented: $alertIsVisible, content: {
-                    let rounded: Int = Int(sliderValue.rounded())
-                    return Alert(title: Text("Nice!"),
-                                 message: Text("Slider value is \(rounded)\n" + "You scored \(game.points(sliderValue: rounded)) points this round"),
-                                 dismissButton: .default(Text("Awesome"))
-                    )
-                })
+                InstructionView(game: $game)
+                SliderView(sliderValue: $sliderValue)
+                HitMeButton(alertIsVisible: $alertIsVisible, sliderValue: $sliderValue, game: $game)
             }
         }
+    }
+}
+
+struct InstructionView: View {
+    @Binding var game: Game
+    var body: some View {
+        VStack {
+            InstructionText(text: "Put the bullseye as close as you can to".uppercased())
+                .padding(.leading, 30.0)
+                .padding(.trailing, 30.0)
+            BigNumberText(text: String(game.target))
+                .padding(.top, 3.0)
+        }
+
+    }
+}
+
+struct HitMeButton: View {
+    @Binding var alertIsVisible: Bool
+    @Binding var sliderValue: Double
+    @Binding var game: Game
+    var body: some View {
+        Button(action: {
+            alertIsVisible = true
+        }) {
+            Text("submit".uppercased()).bold().font(.title3)
+        }
+        .padding(20.0)
+        .background(
+            ZStack {
+                Color("ButtonColor")
+                LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+            }
+        )
+        .foregroundColor(Color.white)
+        .cornerRadius(21.0)
+        .alert(isPresented: $alertIsVisible, content: {
+            let rounded: Int = Int(sliderValue.rounded())
+            return Alert(title: Text("Nice!"),
+                         message: Text("Slider value is \(rounded)\n" + "You scored \(game.points(sliderValue: rounded)) points this round"),
+                         dismissButton: .default(Text("Awesome"))
+            )
+        })
+    }
+}
+
+struct SliderView: View {
+    @Binding var sliderValue: Double
+    var body: some View {
+        HStack {
+            SliderLabelText(text: "1")
+            Slider(value: $sliderValue, in: 1.0...100.0)
+            SliderLabelText(text: "100")
+        }.padding()
     }
 }
 
