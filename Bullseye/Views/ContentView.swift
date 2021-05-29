8824e42842ad2743c3666c9b6917e58387b2bec1
a1mond <kotik.slava2012@gmail.com>
2021-05-30 00:25:18 +0200

    polishing code

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var alertIsVisible: Bool = false
-    @State private var sliderValue: Double = 50.0
-    @State private var game: Game = Game()
+    @State private var alertIsVisible = false
+    @State private var sliderValue = 50.0
+    @State private var game = Game()
     var body: some View {
         VStack {
             Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
@@ -26,20 +26,20 @@ struct ContentView: View {
             HStack {
                 Text("1")
                     .bold()
-                Slider(value: self.$sliderValue, in: 1.0...100.0)
+                Slider(value: $sliderValue, in: 1.0...100.0)
                 Text("100")
                     .bold()
             }
             Button(action: {
-                self.alertIsVisible = true
+                alertIsVisible = true
                 print("Button is pressed.")
             }) {
                 Text("Hit me!")
             }
             .alert(isPresented: $alertIsVisible, content: {
-                let rounded: Int = Int(self.sliderValue.rounded())
+                let rounded: Int = Int(sliderValue.rounded())
                 return Alert(title: Text("Nice!"),
-                             message: Text("Slider value is \(rounded)\n" + "You scored \(self.game.points(sliderValue: rounded)) points this round"),
+                             message: Text("Slider value is \(rounded)\n" + "You scored \(game.points(sliderValue: rounded)) points this round"),
                              dismissButton: .default(Text("Awesome"))
                 )
             })
