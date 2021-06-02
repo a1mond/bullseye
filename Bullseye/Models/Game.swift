738ced85219aeb596c8710385e19e26549c0361b
a1mond <kotik.slava2012@gmail.com>
2021-06-03 00:58:51 +0200

    added leaderboard logic and tests

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -7,10 +7,16 @@
 
 import Foundation
 
+struct LeaderboardEntry {
+    let score: Int
+    let date: Date
+}
+
 struct Game {
     var target = Int.random(in: 1...100)
     var score = 0
     var round = 1
+    var leaderboardEntries: [LeaderboardEntry] = []
     
     func points(sliderValue: Int) -> Int {
         let difference = abs(sliderValue - target)
@@ -26,10 +32,15 @@ struct Game {
         self.score += points
         self.round += 1
         self.target = Int.random(in: 1...100)
+        self.addToLeaderboard(score: points)
     }
     mutating func restart() {
         self.score = 0
         self.round = 1
         self.target = Int.random(in: 1...100)
+    }
+    mutating func addToLeaderboard(score: Int) {
+        self.leaderboardEntries.append(LeaderboardEntry(score: score, date: Date()))
+        self.leaderboardEntries.sort { $0.score > $1.score }
     }
 }
